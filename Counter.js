86b30a297af86400b5269cd996de90c68a2c4e17
partha2212 <parthabiswas1406@example.com>
2026-05-28 11:52:11 +0530

    added counter app

---
+++ Counter.js
@@ -0,0 +1,29 @@
+document.getElementById("countshow").style.color="gray";
+document.body.style.backgroundColor = "black";
+let count=0;
+let countText=document.getElementById("countshow")
+function update(){
+    countText.innerText = count;
+    changeColor();
+}
+function increase(){
+    count++;
+    update();
+}
+function decrease(){
+    count--;
+    update();
+}
+function reset(){
+    count=0;
+    update();
+}
+function changeColor(){
+    if(count>0){
+        document.getElementById("countshow").style.color="green";
+    }else if(count<0){
+        document.getElementById("countshow").style.color="red";
+    }else{
+        document.getElementById("countshow").style.color="white";
+    }
+}
